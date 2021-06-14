3a2cf727cf766a6cf1c86d2a2882f2ca64d8d882
youssef-ashraf-othman <66360369+youssef-ashraf-othman@users.noreply.github.com>
2021-06-14 15:41:50 +0200

    Update form.js

--- form.js
+++ form.js
@@ -63,7 +63,7 @@ function validate() {
         check = false;
     }
 
-    if (!phone.match(/[0-9]{3}-[0-9]{3}-[0-9]{4}/)) {
+    if (!phone.match(/[0-9]{10})) {
         alert("Phone is not valid");
         check = false;
     }
